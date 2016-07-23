94c593ea162a25f70ed86d05ef89f2f614dc377d
Garima Kamboj <garima.kamboj@icloud.com>
2016-07-24 00:14:23 +0530

    home page done without styling

---
+++ client/js/home.controller.js
@@ -0,0 +1,11 @@
+(function(){
+  angular
+    .module('home')
+    .controller('HomeCtrl', function($scope, $http){
+      $http.get('http://localhost:5000/')
+           .then(function(response){
+              $scope.sellerData = response.data;
+           });
+    });
+
+})();
